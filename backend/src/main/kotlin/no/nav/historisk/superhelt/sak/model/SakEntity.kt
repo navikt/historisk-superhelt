cc3be5efbf3380653d8b8271cc8ed4be8aa2a5f6
Hans-Petter.Vadseth <hans-petter.vadseth@nav.no>
2025-09-11 10:56:51 +0200

    hashcode og equals på SakEntity

--- backend/src/main/kotlin/no/nav/historisk/superhelt/sak/model/SakEntity.kt
+++ backend/src/main/kotlin/no/nav/historisk/superhelt/sak/model/SakEntity.kt
@@ -1,6 +1,7 @@
 package no.nav.historisk.superhelt.sak.model
 
 import jakarta.persistence.*
+import org.hibernate.Hibernate
 
 @Entity
 @Table(name = "sak")
@@ -25,5 +26,13 @@ class SakEntity(
     // Oppgaver
     // Brev
 ) {
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (other == null || Hibernate.getClass(this) != Hibernate.getClass(other)) return false
+        other as SakEntity
+
+        return id != null && id == other.id
+    }
+    override fun hashCode(): Int = javaClass.hashCode()
 
 }
