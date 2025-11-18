09d9f3d67208c5e94c5c905444e5a5b587aeddd2
Hans-Petter Vadseth <hans-petter.vadseth@nav.no>
2025-11-18 13:24:56 +0100

    Utbetaling via Helved (#17)
    * Lager consumer på statusmeldinger fra kafka fra hel ved
    
    * Mock av helved i server
    
    * metrikker for utbetaling status

---
+++ backend/src/main/kotlin/no/nav/historisk/superhelt/utbetaling/db/UtbetalingMetrics.kt
@@ -0,0 +1,26 @@
+package no.nav.historisk.superhelt.utbetaling.db
+
+import io.micrometer.core.instrument.Gauge
+import io.micrometer.core.instrument.MeterRegistry
+import no.nav.historisk.superhelt.utbetaling.UtbetalingStatus
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+class UtbetalingMetrics(private val meterRegistry: MeterRegistry) {
+
+
+    @Bean
+    fun utbetalingStatusGaugeMetric(utbetalingRepository: UtbetalingJpaRepository): String {
+        UtbetalingStatus.entries.forEach { status ->
+            Gauge.builder("utbetaling_count") {
+                utbetalingRepository.countByUtbetalingStatus(status).toDouble()
+            }
+                .description("Antall utbetalinger med status $status")
+                .tag("status", status.name)
+                .register(meterRegistry)
+        }
+        return "Utbetaling metrics registered"
+    }
+
+}
---
+++ mocks/mock-server/src/main/kotlin/no/nav/historisk/mock/helved/HelvedConfig.kt
@@ -0,0 +1,28 @@
+package no.nav.historisk.mock.helved
+
+import org.apache.kafka.clients.admin.NewTopic
+import org.springframework.boot.context.properties.ConfigurationProperties
+import org.springframework.boot.context.properties.EnableConfigurationProperties
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.scheduling.annotation.EnableScheduling
+
+@Configuration
+@EnableScheduling
+@EnableConfigurationProperties(HelvedConfigProperties::class)
+class UtbetalingConfig(private val config: HelvedConfigProperties) {
+
+    @Bean
+    fun utbetalingTopic(): NewTopic {
+        return NewTopic(config.utbetalingTopic, 1, 1)
+    }
+
+    @Bean
+    fun statusTopic(): NewTopic {
+        return NewTopic(config.statusTopic, 1, 1)
+    }
+
+}
+
+@ConfigurationProperties(prefix = "app.helved")
+class HelvedConfigProperties(val utbetalingTopic: String, val statusTopic: String)
