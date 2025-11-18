de84d3a43813c2eda924e32aef5e76592fda4fef
Hans-Petter.Vadseth <hans-petter.vadseth@nav.no>
2025-11-18 12:44:09 +0100

    metrikker for utbetaling status

--- backend/src/main/kotlin/no/nav/historisk/superhelt/utbetaling/db/UtbetalingJpaRepository.kt
+++ backend/src/main/kotlin/no/nav/historisk/superhelt/utbetaling/db/UtbetalingJpaRepository.kt
@@ -1,11 +1,14 @@
 package no.nav.historisk.superhelt.utbetaling.db
 
+import no.nav.historisk.superhelt.utbetaling.UtbetalingStatus
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
 import java.util.*
 
 @Repository
 interface UtbetalingJpaRepository : JpaRepository<UtbetalingJpaEntity, Long> {
+
+    fun countByUtbetalingStatus(utbetalingStatus: UtbetalingStatus): Long
 
     fun findByUuid(uuid: UUID): UtbetalingJpaEntity?
 }
