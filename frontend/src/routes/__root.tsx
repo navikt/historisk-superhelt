d401928b8f51b66e82b95cbef22489ff8b61c2d1
Hans-Petter Vadseth <hans-petter.vadseth@nav.no>
2025-12-03 10:48:51 +0100

    div oppdateringer (#27)
    * Oppdaterer til npm 24
    * Fjerner ubrukte npm pakker
    * Oppdaterer npm pakker
    * Oppdaterer spring versjon
    * Prøver å få codeql til å virke

--- frontend/src/routes/__root.tsx
+++ frontend/src/routes/__root.tsx
@@ -1,4 +1,4 @@
-import { createRootRouteWithContext, Outlet} from '@tanstack/react-router'
+import {createRootRouteWithContext, Outlet} from '@tanstack/react-router'
 import {TanStackRouterDevtools} from '@tanstack/react-router-devtools'
 import {Theme} from "@navikt/ds-react/Theme";
 import {ReactQueryDevtools} from "@tanstack/react-query-devtools";
@@ -10,22 +10,22 @@ import {RouterContext} from "~/main";
 export const Route = createRootRouteWithContext<RouterContext>()({
     component: () => (
         <Theme theme={"light"}>
-            <Page footer={<Footer />}  >
-                <RfcErrorBoundary >
-                <Page.Block as="header"  gutters>
-                    <Header />
-                </Page.Block>
-                <Page.Block as="main" gutters>
-                    <Outlet />
-                </Page.Block>
-                </RfcErrorBoundary >
+            <Page footer={<Footer/>}>
+                <RfcErrorBoundary>
+                    <Page.Block as="header" gutters>
+                        <Header/>
+                    </Page.Block>
+                    <Page.Block as="main" gutters>
+                        <Outlet/>
+                    </Page.Block>
+                </RfcErrorBoundary>
             </Page>
-            <TanStackRouterDevtools />
-            <ReactQueryDevtools buttonPosition="bottom-right" />
+            <TanStackRouterDevtools/>
+            <ReactQueryDevtools buttonPosition="bottom-right"/>
         </Theme>
     ),
 })
 
 function Footer() {
-    return <Page.Block as={"footer"} id="decorator-footer" ></Page.Block>;
+    return <Page.Block as={"footer"} id="decorator-footer"></Page.Block>;
 }
