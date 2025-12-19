2b5e38577abfdb827038970710e8ef1c0449eee7
Hans-Petter.Vadseth <hans-petter.vadseth@nav.no>
2025-12-19 08:48:41 +0100

    Småfix

--- frontend/src/routes/sak/$saksnummer/vedtaksbrevbruker.tsx
+++ frontend/src/routes/sak/$saksnummer/vedtaksbrevbruker.tsx
@@ -23,7 +23,4 @@ function BrevPage() {
                               readOnly={!hasSaksbehandleRettighet}
                               onSuccess={gotoOppsummering}/>
 
-
 }
-
-
--- mocks/mock-server/src/main/kotlin/no/nav/historisk/mock/dokarkiv/SafTestdata.kt
+++ mocks/mock-server/src/main/kotlin/no/nav/historisk/mock/dokarkiv/SafTestdata.kt
@@ -13,49 +13,49 @@ fun generateJournalpost(
     journalpostId: EksternJournalpostId,
     antallEkstraVedlegg: Int = 0,
 ): Journalpost {
-   val tittel = "NAV 10-07.10 Søknad om ${faker.food().dish()}"
-   val dokumenter =
-      mutableListOf(
-         dokumentInfo.copy(
-            dokumentInfoId = EksternDokumentInfoId(faker.numerify("#########")),
-            tittel = tittel,
-         ),
-      )
-   repeat(antallEkstraVedlegg) {
-      dokumenter.add(
-         dokumentInfo.copy(
-            dokumentInfoId = EksternDokumentInfoId(faker.numerify("#########")),
-            tittel = "Vedlegg: ${faker.book().title()}",
-         ),
-      )
-   }
-   return journalpost.copy(journalpostId = journalpostId, tittel = tittel,  dokumenter = dokumenter)
+    val tittel = "NAV 10-07.10 Søknad om ${faker.food().dish()}"
+    val dokumenter =
+        mutableListOf(
+            dokumentInfo.copy(
+                dokumentInfoId = EksternDokumentInfoId(faker.numerify("#########")),
+                tittel = tittel,
+            ),
+        )
+    repeat(antallEkstraVedlegg) {
+        dokumenter.add(
+            dokumentInfo.copy(
+                dokumentInfoId = EksternDokumentInfoId(faker.numerify("#########")),
+                tittel = "Vedlegg: ${faker.book().title()}",
+            ),
+        )
+    }
+    return journalpost.copy(journalpostId = journalpostId, tittel = tittel, dokumenter = dokumenter)
 }
 
 val dokumentInfo =
-   JournalpostDokumentInfo(
-      tittel = "NAV 10-07.10 Mockopediske hjelpemidler",
-      dokumentInfoId = EksternDokumentInfoId("454256793"),
-      dokumentvarianter =
-         listOf(
-            JournalpostDokumentVariant(
-               filtype = "PDF",
-               filnavn = "test.pdf",
-               saksbehandlerHarTilgang = true,
+    JournalpostDokumentInfo(
+        tittel = "NAV 10-07.10 Mockopediske hjelpemidler",
+        dokumentInfoId = EksternDokumentInfoId("454256793"),
+        dokumentvarianter =
+            listOf(
+                JournalpostDokumentVariant(
+                    filtype = "PDF",
+                    filnavn = "test.pdf",
+                    saksbehandlerHarTilgang = true,
+                ),
             ),
-         ),
-   )
+    )
 private val journalpost =
-   Journalpost(
-      journalpostId = EksternJournalpostId("453863071"),
-      tittel = "NAV 10-07.10 Ortopediske hjelpemidler",
-      journalstatus = JournalStatus.MOTTATT,
-      bruker =
-         JournalpostBruker(
-            id = "11111111111",
-            type = BrukerIdType.FNR,
-         ),
-      dokumenter = emptyList(),
-   )
+    Journalpost(
+        journalpostId = EksternJournalpostId("453863071"),
+        tittel = "NAV 10-07.10 Ortopediske hjelpemidler",
+        journalstatus = JournalStatus.MOTTATT,
+        bruker =
+            JournalpostBruker(
+                id = "11111111111",
+                type = BrukerIdType.FNR,
+            ),
+        dokumenter = emptyList(),
+    )
 
 val pdfdoc = classpathAsStream("/saf/saf_doc.pdf").readBytes()
