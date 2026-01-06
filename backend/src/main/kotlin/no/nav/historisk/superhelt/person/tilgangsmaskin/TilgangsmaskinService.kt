61b68a3c1417628576b37dc139a79285abf2f543
Hans-Petter Vadseth <hans-petter.vadseth@nav.no>
2026-01-06 08:21:26 +0100

    cache av pdl og tilgangsmaskin (#54)
    * Legger til cache på pdl og tilgangsmaskin
    - cache time ut 20 min etter skriving

--- backend/src/main/kotlin/no/nav/historisk/superhelt/person/tilgangsmaskin/TilgangsmaskinService.kt
+++ backend/src/main/kotlin/no/nav/historisk/superhelt/person/tilgangsmaskin/TilgangsmaskinService.kt
@@ -1,17 +1,23 @@
 package no.nav.historisk.superhelt.person.tilgangsmaskin
 
 import no.nav.common.types.Fnr
+import no.nav.historisk.superhelt.infrastruktur.getCurrentNavIdent
 import no.nav.tilgangsmaskin.TilgangsmaskinClient
+import org.springframework.cache.Cache
 
 
 class TilgangsmaskinService(
     private val tilgangsmaskinClient: TilgangsmaskinClient,
+    private val cache: Cache
 ) {
 
-    //TODO caching?
     fun sjekkKomplettTilgang(fnr: Fnr): TilgangsmaskinClient.TilgangResult {
-        return tilgangsmaskinClient.komplett(fnr.value)
-    }
+        val cacheKey = "${getCurrentNavIdent()}:${fnr.value}"
 
+        return cache.get(cacheKey, TilgangsmaskinClient.TilgangResult::class.java)
+            ?: tilgangsmaskinClient.komplett(fnr.value).also { result ->
+                cache.put(cacheKey, result)
+            }
+    }
 }
 
