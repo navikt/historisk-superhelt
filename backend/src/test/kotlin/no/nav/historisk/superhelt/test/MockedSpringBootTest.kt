23e42b6ff600d587bdc8baaebb76614821ef0587
Hans-Petter.Vadseth <hans-petter.vadseth@nav.no>
2026-01-09 10:20:16 +0100

    Bruker testcontainer for kafka

--- backend/src/test/kotlin/no/nav/historisk/superhelt/test/MockedSpringBootTest.kt
+++ backend/src/test/kotlin/no/nav/historisk/superhelt/test/MockedSpringBootTest.kt
@@ -2,11 +2,9 @@ package no.nav.historisk.superhelt.test
 
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.context.annotation.Import
-import org.springframework.kafka.test.context.EmbeddedKafka
 import org.springframework.test.context.ActiveProfiles
 
 @ActiveProfiles("junit")
 @SpringBootTest
-@EmbeddedKafka(kraft = true, topics = ["\${app.utbetaling.status-topic}", "\${app.utbetaling.utbetaling-topic}"])
-@Import(ExternalMockTestConfig::class, PostgresTestcontainersConfiguration::class)
+@Import(ExternalMockTestConfig::class, PostgresTestcontainersConfiguration::class, KafkaTestcontainersConfiguration::class)
 annotation class MockedSpringBootTest()
