af89a304ea63bbccd2328496f277aa5f05b4209c
havardnyboe <40893697+havardnyboe@users.noreply.github.com>
2026-02-12 11:06:40 +0100

    legg til disable på prosessmenyen

--- frontend/src/common/process-menu/ProcessMenu.tsx
+++ frontend/src/common/process-menu/ProcessMenu.tsx
@@ -1,11 +1,50 @@
-import {HStack} from '@navikt/ds-react';
+import { BodyShort, Box, HStack } from "@navikt/ds-react";
+import { StepIcon } from "./StepIcon";
+import { StepType } from "./StepType";
+import { Link, type LinkProps } from "@tanstack/react-router";
+import styles from "./step.module.css";
 
 interface ProcessMenuProps {
-    children?: React.ReactNode;
+	children?: React.ReactNode;
 }
 
-export const ProcessMenu = ({children}: ProcessMenuProps) => (
-    <HStack gap="space-16">
-        {children}
-    </HStack>
+interface ProcessButtonProps extends LinkProps {
+	label: string;
+	stepType?: StepType;
+	disabled?: boolean;
+}
+
+const ProcessMenu = ({ children }: ProcessMenuProps) => (
+	<HStack gap="space-16">{children}</HStack>
 );
+
+const ProcessMenuItem = ({
+	label,
+	stepType = StepType.default,
+	disabled = false,
+	...linkProps
+}: ProcessButtonProps) => {
+	return (
+		<Link {...linkProps} className={styles.step} disabled={disabled}>
+			{({ isActive }) => {
+				return (
+					<Box
+						className={`${styles.step__button} ${styles[stepType]} ${isActive ? styles["active"] : ""} ${disabled && styles["disabled"]}`}
+					>
+						<StepIcon type={stepType} usePartialStatus={false} />
+						<BodyShort as="span" size="small" className={styles.step__text}>
+							{label}
+						</BodyShort>
+						{isActive && (
+							<div className={`${styles["step__arrow-container"]}`} />
+						)}
+					</Box>
+				);
+			}}
+		</Link>
+	);
+};
+
+ProcessMenu.Item = ProcessMenuItem;
+
+export { ProcessMenu };
--- frontend/src/routes/sak/$saksnummer/route.tsx
+++ frontend/src/routes/sak/$saksnummer/route.tsx
@@ -1,98 +1,103 @@
-import {createFileRoute, Outlet} from '@tanstack/react-router'
-import {Box, HGrid, Tabs, VStack} from '@navikt/ds-react'
-import {PersonHeader} from "~/common/person/PersonHeader";
-import {useSuspenseQuery} from "@tanstack/react-query";
-import {getSakOptions} from "./-api/sak.query";
-import {FilePdfIcon, TasklistIcon} from "@navikt/aksel-icons";
-import {ErrorAlert} from "~/common/error/ErrorAlert";
+import { createFileRoute, Outlet } from "@tanstack/react-router";
+import { Box, HGrid, Tabs, VStack } from "@navikt/ds-react";
+import { PersonHeader } from "~/common/person/PersonHeader";
+import { useSuspenseQuery } from "@tanstack/react-query";
+import { getSakOptions } from "./-api/sak.query";
+import { FilePdfIcon, TasklistIcon } from "@navikt/aksel-icons";
+import { ErrorAlert } from "~/common/error/ErrorAlert";
 import SakHeading from "~/routes/sak/$saksnummer/-components/SakHeading";
-import {StepType} from "~/common/process-menu/StepType";
-import {ProcessMenuItem} from "~/common/process-menu/ProcessMenuItem";
-import {ProcessMenu} from "~/common/process-menu/ProcessMenu";
-import {SakerTable} from "~/routes/person/$personid/-components/SakerTable";
-import {TilstandStatusType} from "~/routes/sak/$saksnummer/-types/sak.types";
+import { StepType } from "~/common/process-menu/StepType";
+import { ProcessMenu } from "~/common/process-menu/ProcessMenu";
+import { SakerTable } from "~/routes/person/$personid/-components/SakerTable";
+import type { TilstandStatusType } from "~/routes/sak/$saksnummer/-types/sak.types";
 import DokumentViewer from "~/routes/sak/$saksnummer/-components/dokumenter/DokumentViewer";
 import SakAlert from "~/routes/sak/$saksnummer/-components/SakAlerts";
 
-export const Route = createFileRoute('/sak/$saksnummer')({
-    component: SakLayout,
-    loader: ({params: {saksnummer}, context}) => {
-        context.queryClient.ensureQueryData(getSakOptions(saksnummer))
-    },
-    errorComponent: ({error}) => {
-        return <ErrorAlert error={error}/>
-    }
-})
+export const Route = createFileRoute("/sak/$saksnummer")({
+	component: SakLayout,
+	loader: ({ params: { saksnummer }, context }) => {
+		context.queryClient.ensureQueryData(getSakOptions(saksnummer));
+	},
+	errorComponent: ({ error }) => {
+		return <ErrorAlert error={error} />;
+	},
+});
 
 function SakLayout() {
-    const {saksnummer} = Route.useParams()
-    const {data: sak} = useSuspenseQuery(getSakOptions(saksnummer))
+	const { saksnummer } = Route.useParams();
+	const { data: sak } = useSuspenseQuery(getSakOptions(saksnummer));
 
+	const calculateStepType = (
+		tilstandResultat: TilstandStatusType,
+	): StepType => {
+		switch (tilstandResultat) {
+			case "IKKE_STARTET":
+				return StepType.default;
+			case "OK":
+				return StepType.success;
+			case "VALIDERING_FEILET":
+				return StepType.warning;
+			default:
+				return StepType.default;
+		}
+	};
 
-    const calculateStepType = (tilstandResultat: TilstandStatusType): StepType => {
-        switch (tilstandResultat) {
-            case "IKKE_STARTET":
-                return StepType.default;
-            case "OK":
-                return StepType.success;
-            case "VALIDERING_FEILET":
-                return StepType.warning;
-            default:
-                return StepType.default;
-        }
-    }
+	return (
+		<>
+			<PersonHeader maskertPersonId={sak.maskertPersonIdent} />
+			<SakAlert sak={sak} />
+			<HGrid gap="space-24" columns={{ lg: 1, xl: 2 }} marginBlock={"space-16"}>
+				<VStack gap="space-16">
+					<ProcessMenu>
+						<ProcessMenu.Item
+							label={"Opplysninger"}
+							stepType={calculateStepType(sak?.tilstand.opplysninger)}
+							to={"/sak/$saksnummer/opplysninger"}
+						/>
+						<ProcessMenu.Item
+							label={"Brev til bruker"}
+							stepType={calculateStepType(sak?.tilstand.vedtaksbrevBruker)}
+							to={"/sak/$saksnummer/vedtaksbrevbruker"}
+							disabled={sak?.tilstand.opplysninger !== "OK"}
+						/>
+						<ProcessMenu.Item
+							label={"Oppsummering"}
+							stepType={calculateStepType(sak?.tilstand.oppsummering)}
+							to={"/sak/$saksnummer/oppsummering"}
+							disabled={sak?.tilstand.vedtaksbrevBruker !== "OK"}
+						/>
+					</ProcessMenu>
 
-    return (
-        <>
-            <PersonHeader maskertPersonId={sak.maskertPersonIdent}/>
-            <SakAlert sak={sak}/>
-            <HGrid gap="space-24" columns={{lg: 1, xl: 2}} marginBlock={"space-16"}>
-                <VStack gap="space-16">
-                    <ProcessMenu>
-                        <ProcessMenuItem label={"Opplysninger"} stepType={calculateStepType(sak?.tilstand.opplysninger)}
-                                         to={"/sak/$saksnummer/opplysninger"}/>
-                        <ProcessMenuItem label={"Brev til bruker"}
-                                         stepType={calculateStepType(sak?.tilstand.vedtaksbrevBruker)}
-                                         to={"/sak/$saksnummer/vedtaksbrevbruker"}/>
-                        <ProcessMenuItem label={"Oppsummering"}
-                                         stepType={calculateStepType(sak?.tilstand.oppsummering)}
-                                         to={"/sak/$saksnummer/oppsummering"}/>
-
-                    </ProcessMenu>
-
-                    <Outlet/>
-
-                </VStack>
-                <VStack gap="space-16">
-                    <SakHeading sak={sak}/>
-                    <Tabs defaultValue="dokumenter">
-                        <Tabs.List>
-                            <Tabs.Tab
-                                value="dokumenter"
-                                label="Dokumenter"
-                                icon={<FilePdfIcon aria-hidden/>}
-                            />
-                            <Tabs.Tab
-                                value="historikk"
-                                label="Sakshistorikk"
-                                icon={<TasklistIcon aria-hidden/>}
-                            />
-
-                        </Tabs.List>
-                        <Tabs.Panel value="dokumenter">
-                            <Box width="100%" height="6rem" padding="space-16">
-                                <DokumentViewer saksnummer={saksnummer}/>
-                            </Box>
-                        </Tabs.Panel>
-                        <Tabs.Panel value="historikk">
-                            <Box width="100%" height="6rem" padding="space-16">
-                                <SakerTable maskertPersonIdent={sak.maskertPersonIdent}/>
-                            </Box>
-                        </Tabs.Panel>
-                    </Tabs>
-                </VStack>
-
-            </HGrid>
-        </>
-    )
+					<Outlet />
+				</VStack>
+				<VStack gap="space-16">
+					<SakHeading sak={sak} />
+					<Tabs defaultValue="dokumenter">
+						<Tabs.List>
+							<Tabs.Tab
+								value="dokumenter"
+								label="Dokumenter"
+								icon={<FilePdfIcon aria-hidden />}
+							/>
+							<Tabs.Tab
+								value="historikk"
+								label="Sakshistorikk"
+								icon={<TasklistIcon aria-hidden />}
+							/>
+						</Tabs.List>
+						<Tabs.Panel value="dokumenter">
+							<Box width="100%" height="6rem" padding="space-16">
+								<DokumentViewer saksnummer={saksnummer} />
+							</Box>
+						</Tabs.Panel>
+						<Tabs.Panel value="historikk">
+							<Box width="100%" height="6rem" padding="space-16">
+								<SakerTable maskertPersonIdent={sak.maskertPersonIdent} />
+							</Box>
+						</Tabs.Panel>
+					</Tabs>
+				</VStack>
+			</HGrid>
+		</>
+	);
 }
