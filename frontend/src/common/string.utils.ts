592050becbd87dcc7d68f177508ed53e7ab466b8
havardnyboe <40893697+havardnyboe@users.noreply.github.com>
2026-02-20 15:47:34 +0100

    setter locale

--- frontend/src/common/string.utils.ts
+++ frontend/src/common/string.utils.ts
@@ -2,8 +2,8 @@ export function enumkodeTilTekst(enumKode: string | undefined, storBokstav = tru
     return enumKode
         ?.split(separator)
         .map((del, index) => {
-            const lower = del.toLocaleLowerCase();
-            return storBokstav && index === 0 ? lower.charAt(0).toLocaleUpperCase() + lower.slice(1) : lower;
+            const lower = del.toLocaleLowerCase("nb-NO");
+            return storBokstav && index === 0 ? lower.charAt(0).toLocaleUpperCase("nb-NO") + lower.slice(1) : lower;
         })
         .join(" ");
 }
