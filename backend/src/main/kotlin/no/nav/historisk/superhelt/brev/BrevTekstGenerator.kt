58bbaf999edef9911ba0837a127920c80e1f3eae
Hans-Petter Vadseth <hans-petter.vadseth@nav.no>
2026-03-02 16:10:18 +0100

    Litt bedre brevtekster (#133)
    * Litt bedre brevtekster

--- backend/src/main/kotlin/no/nav/historisk/superhelt/brev/BrevTekstGenerator.kt
+++ backend/src/main/kotlin/no/nav/historisk/superhelt/brev/BrevTekstGenerator.kt
@@ -20,23 +20,23 @@ class BrevTekstGenerator(private val sak: Sak) {
 
     private fun generateHenleggelsebrev(): String {
         return """
-            <h1>Henleggelsebrev</h1>
-            <p>Søknaden av ${sak.soknadsDato?.format(navDateFormatter)} om ${sak.type.navn.lowercase()} er henlagt.</p>
+            <h1>Søknad om ${sak.type.navn.lowercase()} er henlagt</h1>
+            <p>Søknad datert ${sak.soknadsDato?.format(navDateFormatter)} om ${sak.type.navn.lowercase()} er henlagt.</p>
         """.trimIndent()
     }
 
     private fun generateVedtaksbrev(mottaker: BrevMottaker): String {
         when (mottaker) {
             BrevMottaker.BRUKER -> {
+                val showUtbetaling =
+                    sak.utbetalingsType == UtbetalingsType.BRUKER && sak.vedtaksResultat?.isInnvilget() == true && sak.belop != null
+
                 return """
-                    <h1>Vedtaksbrev</h1>
+                    <h1>Vedtak om ${sak.type.navn.lowercase()}</h1>
                     <p>Søknaden din av ${sak.soknadsDato?.format(navDateFormatter)} om ${sak.type.navn.lowercase()} er ${sak.vedtaksResultat?.navn}.</p>
-                 
-                    <p>For ${sak.tildelingsAar} får du</p>
-                    <ul>
-                        ${if (sak.utbetalingsType == UtbetalingsType.BRUKER && sak.belop != null) "<li>Utbetaling: ${sak.belop} kr til din konto</li>" else ""}
-                        ${if (sak.utbetalingsType == UtbetalingsType.FORHANDSTILSAGN && sak.belop != null) "<li>Forhåndstilsagn: ${sak.belop} kr</li>" else ""}
-                      </ul> 
+                    <p></p>
+                    ${if (showUtbetaling) "<p>Vi har utbetalt ${sak.belop} kr til konto som er registrert hos Nav. Pengene vil være på din konto innen 1-4 virkedager</p>" else ""}
+                           
                 """.trimIndent()
             }
 
@@ -57,12 +57,23 @@ class BrevTekstGenerator(private val sak: Sak) {
 
     fun generateTittel(type: BrevType, mottaker: BrevMottaker): String {
         return when (type) {
-            BrevType.VEDTAKSBREV -> when (mottaker) {
-                BrevMottaker.BRUKER -> "Vedtaksbrev, ${sak.vedtaksResultat?.navn?.lowercase()} for ${sak.type.navn.lowercase()}"
-                BrevMottaker.SAMHANDLER -> "Vedtaksbrev til samhandler, ${sak.vedtaksResultat?.navn?.lowercase()} for ${sak.type.navn.lowercase()}"
-            }
+            BrevType.VEDTAKSBREV -> generateVedtaksbrevTittel(mottaker)
             BrevType.HENLEGGESEBREV -> "Henleggelsebrev for ${sak.type.navn.lowercase()}"
             BrevType.FRITEKSTBREV -> ""
+        }
+    }
+
+    private fun generateVedtaksbrevTittel(mottaker: BrevMottaker): String {
+        return when (mottaker) {
+            BrevMottaker.BRUKER -> {
+                if (sak.gjenapnet) {
+                    "Korrigert vedtaksbrev om ${sak.type.navn.lowercase()}, ${sak.vedtaksResultat?.navn?.lowercase()}"
+                } else {
+                    "Vedtaksbrev om ${sak.type.navn.lowercase()}, ${sak.vedtaksResultat?.navn?.lowercase()}"
+                }
+            }
+
+            BrevMottaker.SAMHANDLER -> "Vedtaksbrev til samhandler om ${sak.type.navn.lowercase()}, ${sak.vedtaksResultat?.navn?.lowercase()}"
         }
     }
 
