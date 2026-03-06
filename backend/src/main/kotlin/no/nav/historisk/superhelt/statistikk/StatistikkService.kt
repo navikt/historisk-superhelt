fb35bae72ce3cc683371e28854c241376e29984a
Hans-Petter.Vadseth <hans-petter.vadseth@nav.no>
2026-03-06 14:36:42 +0100

    Finner appversion fra nais

--- backend/src/main/kotlin/no/nav/historisk/superhelt/statistikk/StatistikkService.kt
+++ backend/src/main/kotlin/no/nav/historisk/superhelt/statistikk/StatistikkService.kt
@@ -1,9 +1,14 @@
 package no.nav.historisk.superhelt.statistikk
 
 import no.nav.historisk.superhelt.statistikk.kafka.SakStatistikkKafkaProducer
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Service
 
 @Service
-class StatistikkService(private val statistikkKafkaProducer: SakStatistikkKafkaProducer) {
+class StatistikkService(
+    private val statistikkKafkaProducer: SakStatistikkKafkaProducer,
+    @param:Value("\${info.app.image}") private val appVersion: String) {
+
+
 
 }
