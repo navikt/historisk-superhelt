bc78af4f7bdea03d3384abe08e0292446ada770a
havardnyboe <40893697+havardnyboe@users.noreply.github.com>
2026-03-13 11:25:24 +0100

    justerer posisjonen til personheader

--- frontend/src/common/person/PersonHeader.tsx
+++ frontend/src/common/person/PersonHeader.tsx
@@ -1,5 +1,5 @@
 import { PersonIcon } from "@navikt/aksel-icons";
-import { Alert, BodyShort, Box, CopyButton, HStack, Link, Tag, Tooltip } from "@navikt/ds-react";
+import { Alert, Bleed, BodyShort, Box, CopyButton, HStack, Link, Tag, Tooltip } from "@navikt/ds-react";
 import { useSuspenseQuery } from "@tanstack/react-query";
 import { Link as RouterLink } from "@tanstack/react-router";
 import { isoTilLokal } from "~/common/dato.utils";
@@ -27,70 +27,66 @@ export function PersonHeader({ maskertPersonId }: Props) {
     const harBeskyttetAdresse = person.adressebeskyttelseGradering && person.adressebeskyttelseGradering !== "UGRADERT";
 
     return (
-        <Box
-            background="neutral-moderate"
-            paddingInline="space-24 space-0"
-            height="3.5rem"
-            style={{ marginInline: "calc(var(--__axc-page-padding-inline) * -1)" }} // Negativ margin for å kompensere for gutter på Page.Block
-            asChild
-        >
-            <HStack gap="space-8" align="center" justify="start">
-                <HStack gap="space-12" align="center" justify="space-between">
-                    <HStack gap="space-4" align="center" justify="start">
-                        <PersonIcon fontSize="1.5rem" />
-                        <BodyShort weight="semibold">
-                            {person?.navn}
-                            {!person.doed && ` (${person.alder ?? "-"} år)`}
-                        </BodyShort>
+        <Bleed marginInline="full" reflectivePadding asChild>
+            <Box background="neutral-moderate" paddingInline="space-24 space-0" height="3.5rem" asChild>
+                <HStack gap="space-8" align="center" justify="start">
+                    <HStack gap="space-12" align="center" justify="space-between">
+                        <HStack gap="space-4" align="center" justify="start">
+                            <PersonIcon fontSize="1.5rem" />
+                            <BodyShort weight="semibold">
+                                {person?.navn}
+                                {!person.doed && ` (${person.alder ?? "-"} år)`}
+                            </BodyShort>
+                        </HStack>
+                        <BodyShort>/</BodyShort>
+                        <HStack gap="space-2" align="center" justify="start">
+                            <Link as={RouterLink} to={`/person/${person.maskertPersonident}`}>
+                                {person.fnr}
+                            </Link>
+                            <Tooltip content="Kopier fødselsnummer" placement="bottom">
+                                <CopyButton size="small" copyText={person.fnr} />
+                            </Tooltip>
+                        </HStack>
                     </HStack>
-                    <BodyShort>/</BodyShort>
-                    <HStack gap="space-2" align="center" justify="start">
-                        <Link as={RouterLink} to={`/person/${person.maskertPersonident}`}>
-                            {person.fnr}
-                        </Link>
-                        <Tooltip content="Kopier fødselsnummer" placement="bottom">
-                            <CopyButton size="small" copyText={person.fnr} />
-                        </Tooltip>
-                    </HStack>
-                </HStack>
 
-                {person.avvisningsBegrunnelse && (
-                    <Alert variant="error" size="small">
-                        {person.avvisningsBegrunnelse}
-                    </Alert>
-                )}
-                {person.doed && (
-                    <Tooltip content={`Brukeren døde ${isoTilLokal(person.doedsfall)}`} placement="bottom">
-                        <Tag data-color="neutral" variant="outline" size="small">
-                            Dødsdato: {isoTilLokal(person.doedsfall)}
-                        </Tag>
-                    </Tooltip>
-                )}
-                {harBeskyttetAdresse && (
-                    <Tooltip
-                        content={`Brukeren har adressebeskyttelse med gradering ${enumkodeTilTekst(person.adressebeskyttelseGradering, false)}`}
-                        placement="bottom"
-                    >
-                        <Tag
-                            data-color={adressebeskyttelseVariant(person.adressebeskyttelseGradering)}
-                            variant="outline"
-                            size="small"
+                    {person.avvisningsBegrunnelse && (
+                        <Alert variant="error" size="small">
+                            {person.avvisningsBegrunnelse}
+                        </Alert>
+                    )}
+                    {person.doed && (
+                        <Tooltip content={`Brukeren døde ${isoTilLokal(person.doedsfall)}`} placement="bottom">
+                            <Tag data-color="neutral" variant="outline" size="small">
+                                Dødsdato: {isoTilLokal(person.doedsfall)}
+                            </Tag>
+                        </Tooltip>
+                    )}
+                    {harBeskyttetAdresse && (
+                        <Tooltip
+                            content={`Brukeren har adressebeskyttelse med gradering ${enumkodeTilTekst(person.adressebeskyttelseGradering, false)}`}
+                            placement="bottom"
                         >
-                            {enumkodeTilTekst(person.adressebeskyttelseGradering)}
-                        </Tag>
-                    </Tooltip>
-                )}
-                {person.harVerge && (
-                    <Tooltip
-                        content={`${person.vergeInfo?.navn} er verge eller fullmektig for brukeren`}
-                        placement="bottom"
-                    >
-                        <Tag data-color="info" variant="outline" size="small">
-                            Verge: {person.vergeInfo?.navn}
-                        </Tag>
-                    </Tooltip>
-                )}
-            </HStack>
-        </Box>
+                            <Tag
+                                data-color={adressebeskyttelseVariant(person.adressebeskyttelseGradering)}
+                                variant="outline"
+                                size="small"
+                            >
+                                {enumkodeTilTekst(person.adressebeskyttelseGradering)}
+                            </Tag>
+                        </Tooltip>
+                    )}
+                    {person.harVerge && (
+                        <Tooltip
+                            content={`${person.vergeInfo?.navn} er verge eller fullmektig for brukeren`}
+                            placement="bottom"
+                        >
+                            <Tag data-color="info" variant="outline" size="small">
+                                Verge: {person.vergeInfo?.navn}
+                            </Tag>
+                        </Tooltip>
+                    )}
+                </HStack>
+            </Box>
+        </Bleed>
     );
 }
