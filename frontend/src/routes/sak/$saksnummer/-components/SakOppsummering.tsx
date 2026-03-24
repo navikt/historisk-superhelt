9d352702e71f29337c273ba6701180bf67c57d6f
Håvard Nybøe <40893697+havardnyboe@users.noreply.github.com>
2026-03-24 08:40:54 +0100

    Forbedre saksoppsummering og diverse finpuss (#180)
    * juster ikon og legg til kant
    * juster ikon
    * juster farger
    * flytt behandlingsmeny
    * sett primærknapp
    * forbedre visning av saksoppsummering

--- frontend/src/routes/sak/$saksnummer/-components/SakOppsummering.tsx
+++ frontend/src/routes/sak/$saksnummer/-components/SakOppsummering.tsx
@@ -1,37 +1,64 @@
 import type {Sak} from "@generated";
-import {BodyShort, Detail, Heading, HStack, Tag, VStack} from "@navikt/ds-react";
-import {isSakFerdig} from "~/common/sak/sak.utils";
+import {Box, Detail, Heading, HGrid, HStack, Tag, VStack} from "@navikt/ds-react";
+import {isoTilLokal} from "~/common/dato.utils";
+import {isSakFerdig, utbetalingText, vedtakAvslatt} from "~/common/sak/sak.utils";
+import {useSakVedtakNavn} from "~/common/sak/useSakVedtakNavn";
 import {useStonadsTypeNavn} from "~/common/sak/useStonadsTypeNavn";
-import BehandlingsMeny from "~/routes/sak/$saksnummer/-components/BehandlingsMeny";
+import {formatertValuta} from "~/common/string.utils";
+import styles from "~/routes/sak/$saksnummer/-components/SakOppsummering.module.css";
 import SakStatus from "~/routes/sak/$saksnummer/-components/SakStatus";
 
 interface Props {
     sak: Sak;
 }
 
-export default function SakOppsummering({sak}: Props) {
+export default function SakOppsummering({ sak }: Props) {
     const getStonadsTypeNavn = useStonadsTypeNavn();
+    const getSakVedtakNavn = useSakVedtakNavn();
+
+    const utbetalingsType = vedtakAvslatt(sak) ? null : sak.utbetalingsType;
+
+    const textUtbetaling = () => {
+        if (vedtakAvslatt(sak)) {
+            return "-";
+        }
+        return utbetalingText(sak.utbetalingsType);
+    };
+
     return (
-        <HStack justify={"space-between"}>
-            <VStack gap="space-4">
-                <HStack gap={"space-8"} align={"center"}>
-                    <Heading size={"small"}>{sak.beskrivelse}</Heading>
-                </HStack>
-                <HStack gap="space-8" align={"center"}>
-                    <Detail>{getStonadsTypeNavn(sak.type)}</Detail>
-                    <Detail textColor={"subtle"}>{sak.saksnummer}</Detail>
-                    <SakStatus sak={sak}/>
+        <VStack gap="space-8" className={styles.container}>
+            <Box borderColor="neutral-subtle" borderWidth="0 0 1 0" asChild>
+                <HStack gap="space-8" align="center" paddingBlock="space-4">
+                    <Heading size="small">{sak.beskrivelse}</Heading>
+                    <Tag variant="outline" data-color={"info"} size="small">
+                        {getStonadsTypeNavn(sak.type)}
+                    </Tag>
+                    <SakStatus sak={sak} />
                     {!isSakFerdig(sak) && sak.gjenapnet && (
-                        <Tag variant="moderate" size="small" data-color="success">
+                        <Tag variant="outline" size="small" data-color="accent">
                             Gjenåpnet
                         </Tag>
                     )}
                 </HStack>
-                <BodyShort>Utbetales</BodyShort>
-            </VStack>
-            <HStack gap={"space-8"} height={"1.5rem"}>
-                <BehandlingsMeny sak={sak}/>
-            </HStack>
-        </HStack>
+            </Box>
+
+            <HGrid className={styles.grid}>
+                <Detail textColor="subtle">Søknadsdato:</Detail>
+                <Detail>{isoTilLokal(sak.soknadsDato)}</Detail>
+
+                <Detail textColor="subtle">Resultat:</Detail>
+                <Detail>{sak.vedtaksResultat ? getSakVedtakNavn(sak.vedtaksResultat) : "-"}</Detail>
+
+                <Detail textColor="subtle">Utbetaling:</Detail>
+                <Detail>{textUtbetaling()}</Detail>
+
+                {utbetalingsType === "BRUKER" && (
+                    <>
+                        <Detail textColor="subtle">Beløp:</Detail>
+                        <Detail>{formatertValuta(sak.belop)}</Detail>
+                    </>
+                )}
+            </HGrid>
+        </VStack>
     );
 }
