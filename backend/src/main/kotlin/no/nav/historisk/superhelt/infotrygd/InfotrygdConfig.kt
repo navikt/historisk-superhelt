16d7435cdfa18b4f7e656f5d20b462f43cee6471
Hans-Petter.Vadseth <hans-petter.vadseth@nav.no>
2026-03-27 14:32:30 +0100

    Bytter til m2m token for for infotrygd

--- backend/src/main/kotlin/no/nav/historisk/superhelt/infotrygd/InfotrygdConfig.kt
+++ backend/src/main/kotlin/no/nav/historisk/superhelt/infotrygd/InfotrygdConfig.kt
@@ -2,7 +2,7 @@ package no.nav.historisk.superhelt.infotrygd
 
 import no.nav.entraproxy.InfotrygdClient
 import no.nav.historisk.superhelt.infrastruktur.mdc.CallIdClientRequestInterceptor
-import no.nav.historisk.superhelt.infrastruktur.token.NaisTokenClientRequestInterceptor
+import no.nav.historisk.superhelt.infrastruktur.token.M2mNaisTokenClientRequestInterceptor
 import no.nav.historisk.superhelt.infrastruktur.token.NaisTokenService
 import org.springframework.boot.context.properties.ConfigurationProperties
 import org.springframework.boot.context.properties.EnableConfigurationProperties
@@ -19,7 +19,7 @@ class InfotrygdConfig {
         val restClient = RestClient.builder()
             .baseUrl(properties.url)
             .requestInterceptor(CallIdClientRequestInterceptor("Nav-Call-Id"))
-            .requestInterceptor(NaisTokenClientRequestInterceptor(tokenService, properties.audience))
+            .requestInterceptor(M2mNaisTokenClientRequestInterceptor(tokenService, properties.audience))
             .build()
 
         return InfotrygdClient(restClient)
