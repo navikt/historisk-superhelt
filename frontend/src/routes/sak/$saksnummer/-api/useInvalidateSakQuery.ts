8da97c80842bae618d7b0a5b3187cb3e996cded8
Hans-Petter.Vadseth <hans-petter.vadseth@nav.no>
2026-04-09 13:35:42 +0200

    Refresh på dokumenter etter sending

--- frontend/src/routes/sak/$saksnummer/-api/useInvalidateSakQuery.ts
+++ frontend/src/routes/sak/$saksnummer/-api/useInvalidateSakQuery.ts
@@ -1,15 +1,21 @@
-import { hentEndringsloggForSakQueryKey } from "@generated/@tanstack/react-query.gen";
-import { useQueryClient } from "@tanstack/react-query";
-import { sakQueryKey } from "~/routes/sak/$saksnummer/-api/sak.query";
+import {finnJournalposterForSakQueryKey, hentEndringsloggForSakQueryKey} from "@generated/@tanstack/react-query.gen";
+import {useQueryClient} from "@tanstack/react-query";
+import {sakQueryKey} from "~/routes/sak/$saksnummer/-api/sak.query";
 
 /** Invaliderer cachen for en sak og tilhørende endringslogg */
 export function useInvalidateSakQuery() {
     const queryClient = useQueryClient();
 
     function invalidateSakQuery(saksnummer: string) {
+        // Sak
         queryClient.invalidateQueries({ queryKey: sakQueryKey(saksnummer) });
+        // Endringsloggen
         queryClient.invalidateQueries({
             queryKey: hentEndringsloggForSakQueryKey({ path: { saksnummer: saksnummer } }),
+        });
+        // dokumenter knyttet til saken
+        queryClient.invalidateQueries({
+            queryKey: finnJournalposterForSakQueryKey({ path: { saksnummer: saksnummer } }),
         });
     }
 
