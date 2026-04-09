87ae734ab1c48824c48ba50d978741bfd5200dcb
Hans-Petter.Vadseth <hans-petter.vadseth@nav.no>
2026-04-09 13:21:09 +0200

    Lagrer brev i jounralføringsmock

--- mocks/mock-server/src/main/kotlin/no/nav/historisk/mock/dokarkiv/DokarkivTestRepository.kt
+++ mocks/mock-server/src/main/kotlin/no/nav/historisk/mock/dokarkiv/DokarkivTestRepository.kt
@@ -8,29 +8,46 @@ import org.springframework.stereotype.Component
 @Component
 class DokarkivTestRepository {
     private val logger = LoggerFactory.getLogger(javaClass)
-    private val repository = mutableMapOf<EksternJournalpostId, Journalpost>()
+    private val repository = mutableMapOf<EksternJournalpostId, JournalpostMedPdf>()
 
     fun lagre(
         journalpostId: EksternJournalpostId,
-        data: Journalpost,
+        journalpost: Journalpost,
+        pdf: ByteArray?,
     ): EksternJournalpostId {
-        logger.debug("Lagrer journalpostId {} med data {}", journalpostId, data)
-        repository[journalpostId] = data
+        logger.debug("Lagrer journalpostId {} med data {}", journalpostId, journalpost)
+        repository[journalpostId] = JournalpostMedPdf(journalpost, pdf)
         return journalpostId
     }
 
+    fun oppdater(
+        journalpostId: EksternJournalpostId,
+        journalpost: Journalpost,
+    ): EksternJournalpostId {
+        logger.debug("Oppdaterer journalpostId {} med data {}", journalpostId, journalpost)
+        repository[journalpostId]?.let {
+            repository[journalpostId] = it.copy(journalpost = journalpost)
+        }
+        return journalpostId
+    }
+
+    fun getPdf(journalpostId: EksternJournalpostId): ByteArray =
+        repository[journalpostId]?.pdf ?: pdfdoc
+
     fun findOrCreate(journalpostId: EksternJournalpostId): Journalpost =
-        repository[journalpostId]
+        repository[journalpostId]?.journalpost
             ?: generateAndCacheResponse(journalpostId)
 
     fun finnJournalposterForSak(saksnummer: String): List<Journalpost> =
-        repository.values.filter { it.sak?.fagsakId == saksnummer }
+        repository.values.map { it.journalpost }.filter { it.sak?.fagsakId == saksnummer }
 
     private fun generateAndCacheResponse(journalpostId: EksternJournalpostId): Journalpost {
         val harVedlegg = faker.number().numberBetween(0, 100) > 70
         val response = generateJournalpost(journalpostId, if (harVedlegg) 2 else 0)
-        lagre(journalpostId, response)
+        lagre(journalpostId, response, null)
         logger.debug("Registerer ny journalpost i saf-mock: {} -> {}", journalpostId, response)
         return response
     }
+
+    data class JournalpostMedPdf(val journalpost: Journalpost, val pdf: ByteArray? = null)
 }
