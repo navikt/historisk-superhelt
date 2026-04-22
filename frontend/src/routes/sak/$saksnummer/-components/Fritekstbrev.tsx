b38c1509323712baac0c04d40ba2f3d3ffc4471b
Hans-Petter Vadseth <hans-petter.vadseth@nav.no>
2026-04-22 12:04:03 +0200

    Legger til sakrettighet Fritekstbrev (#223)

--- frontend/src/routes/sak/$saksnummer/-components/Fritekstbrev.tsx
+++ frontend/src/routes/sak/$saksnummer/-components/Fritekstbrev.tsx
@@ -1,11 +1,11 @@
-import { sendBrevMutation } from "@generated/@tanstack/react-query.gen";
-import { Dialog, ErrorSummary } from "@navikt/ds-react";
-import { useMutation, useQuery, useSuspenseQuery } from "@tanstack/react-query";
-import { useParams } from "@tanstack/react-router";
-import { getOrCreateBrevOptions } from "~/routes/sak/$saksnummer/-api/brev.query";
-import { getSakOptions } from "~/routes/sak/$saksnummer/-api/sak.query";
-import { useInvalidateSakQuery } from "~/routes/sak/$saksnummer/-api/useInvalidateSakQuery";
-import { BrevEditor } from "~/routes/sak/$saksnummer/-components/BrevEditor";
+import {sendBrevMutation} from "@generated/@tanstack/react-query.gen";
+import {Dialog, ErrorSummary} from "@navikt/ds-react";
+import {useMutation, useQuery, useSuspenseQuery} from "@tanstack/react-query";
+import {useParams} from "@tanstack/react-router";
+import {getOrCreateBrevOptions} from "~/routes/sak/$saksnummer/-api/brev.query";
+import {getSakOptions} from "~/routes/sak/$saksnummer/-api/sak.query";
+import {useInvalidateSakQuery} from "~/routes/sak/$saksnummer/-api/useInvalidateSakQuery";
+import {BrevEditor} from "~/routes/sak/$saksnummer/-components/BrevEditor";
 
 interface FritekstBrevProps {
     open: boolean;
@@ -16,7 +16,7 @@ export function FritekstBrev({ open, onOpenChange }: FritekstBrevProps) {
     const { saksnummer } = useParams({ from: "/sak/$saksnummer" });
     const { data: sak } = useSuspenseQuery(getSakOptions(saksnummer));
     const kanSendeBrev =
-        sak.rettigheter.includes("SAKSBEHANDLE") || sak.rettigheter.includes("SEND_KLAGE");
+        sak.rettigheter.includes("FRITEKSTBREV") ;
     const { data: brev } = useQuery({
         ...getOrCreateBrevOptions(saksnummer, "FRITEKSTBREV", "BRUKER"),
         enabled: open,
