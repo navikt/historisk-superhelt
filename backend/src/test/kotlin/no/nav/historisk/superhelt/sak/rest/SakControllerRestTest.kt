52c64405405eb7c61564184d80eae55990ece267
Hans-Petter.Vadseth <hans-petter.vadseth@nav.no>
2026-05-04 13:02:44 +0200

    Tester for klassekoder

--- backend/src/test/kotlin/no/nav/historisk/superhelt/sak/rest/SakControllerRestTest.kt
+++ backend/src/test/kotlin/no/nav/historisk/superhelt/sak/rest/SakControllerRestTest.kt
@@ -3,6 +3,8 @@ package no.nav.historisk.superhelt.sak.rest
 import no.nav.common.types.Belop
 import no.nav.common.types.FolkeregisterIdent
 import no.nav.common.types.Saksnummer
+import no.nav.helved.KlasseKode
+import no.nav.historisk.superhelt.StonadsType
 import no.nav.historisk.superhelt.person.TilgangsmaskinTestData
 import no.nav.historisk.superhelt.person.tilgangsmaskin.TilgangsmaskinService
 import no.nav.historisk.superhelt.person.toMaskertPersonIdent
@@ -13,6 +15,7 @@ import no.nav.historisk.superhelt.test.MockedSpringBootTest
 import no.nav.historisk.superhelt.test.WithLeseBruker
 import no.nav.historisk.superhelt.test.WithSaksbehandler
 import no.nav.historisk.superhelt.test.bodyAsProblemDetail
+import no.nav.historisk.superhelt.test.withMockedUser
 import no.nav.historisk.superhelt.utbetaling.UtbetalingRepository
 import no.nav.historisk.superhelt.utbetaling.UtbetalingStatus
 import no.nav.historisk.superhelt.utbetaling.UtbetalingsType
@@ -21,7 +24,11 @@ import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
-import org.mockito.kotlin.*
+import org.mockito.kotlin.any
+import org.mockito.kotlin.atLeast
+import org.mockito.kotlin.doReturn
+import org.mockito.kotlin.verify
+import org.mockito.kotlin.whenever
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.webmvc.test.autoconfigure.AutoConfigureMockMvc
 import org.springframework.http.HttpStatus
@@ -86,6 +93,30 @@ class SakControllerRestTest() {
                     assertThat(it.fnr).isNotNull
                 })
             verify(tilgangsmaskinService, atLeast(1)).sjekkKomplettTilgang(opprettetSak.fnr)
+        }
+
+        @Test
+        fun `oppdater sak med utbetaling og klassekode`() {
+            val opprettetSak = SakTestData.lagreSak(repository, SakTestData.sakUtenUtbetaling().copy(type = StonadsType.HOREAPPARAT))
+            val saksnummer = opprettetSak.saksnummer
+
+            assertThat(
+                oppdaterSak(
+                    saksnummer, SakUpdateRequestDto(
+                        utbetalingsType = UtbetalingsType.BRUKER,
+                        belop = Belop(5000),
+                        klasseKode = KlasseKode.HØREAPPARAT_EGENBETALING
+                    )
+                )
+            )
+                .hasStatus(HttpStatus.OK)
+
+            val sak = withMockedUser { repository.getSak(saksnummer) }
+            assertThat(sak.saksnummer).isEqualTo(saksnummer)
+            assertThat(sak.fnr).isNotNull
+            assertThat(sak.utbetalingsType).isEqualTo(UtbetalingsType.BRUKER)
+            assertThat(sak.belop).isEqualTo(Belop(5000))
+            assertThat(sak.klasseKode).isEqualTo(KlasseKode.HØREAPPARAT_EGENBETALING)
         }
 
         @WithLeseBruker
